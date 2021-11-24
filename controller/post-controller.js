2e9ed46290616c4fbcef74d301e85dc8a79c4985
prosourav <sourav19961010@gmail.com>
2021-11-24 19:30:06 +0530

    creating updatePost

--- controller/post-controller.js
+++ controller/post-controller.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import Post from "../schema/post-schema.js";
 
 export const createPost = async (request, response) => {
@@ -6,6 +7,33 @@ export const createPost = async (request, response) => {
     const post = await new Post(request.body);
     post.save();
     response.status(200).json("Blog saved successfully");
+  } catch (error) {
+    response.status(500).json(error);
+  }
+};
+
+export const getAllPosts = async (request, response) => {
+  try {
+    const posts = await Post.find({});
+    response.status(200).json(posts);
+  } catch (error) {
+    response.status(500).json(error);
+  }
+};
+
+export const getPost = async (request, response) => {
+  try {
+    const post = await Post.findById(request.params.id);
+    response.status(200).json(post);
+  } catch (error) {
+    response.status(500).json(error);
+  }
+};
+
+export const updatePost = async (request, response) => {
+  try {
+    const newPost = await Post.findById(request.params.id);
+    response.status(200).json(newPost);
   } catch (error) {
     response.status(500).json(error);
   }
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { createPost } from "../controller/post-controller.js";
+import {
+  createPost,
+  getAllPosts,
+  getPost,
+  updatePost,
+} from "../controller/post-controller.js";
 
 const router = express.Router();
 
 router.post("/create", createPost);
+router.get("/posts", getAllPosts);
+router.get("/post/:id", getPost);
+router.get("/update/:id", updatePost);
 
 export default router;
