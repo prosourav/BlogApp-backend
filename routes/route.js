79b40ddb953c2cc76bfd97925470a90de3858ebc
prosourav <sourav19961010@gmail.com>
2021-11-27 22:09:35 +0530

    v2

--- routes/route.js
+++ routes/route.js
@@ -22,7 +22,7 @@ router.get("/post/:id", getPost);
 router.post("/update/:id", updatePost);
 router.delete("/delete/:id", deletePost);
 router.post("/file/upload", upload.single("file"), uploadImage);
-router.get("//file/:filename", getImage);
+router.get("/file/:filename", getImage);
 router.post("/comment/new", postComment);
 router.get("/comment/:id", getComments);
 router.delete("/comment/delete/:id", deleteComment);
